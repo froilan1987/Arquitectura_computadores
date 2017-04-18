cf0cbd1efb2d912b28838d1d60df40f7933d8e78
froilan1987 <froilanba@utp.edu.co>
2017-04-18 00:23:21 -0500

    1

---
+++ procesadores/Procesador2/procesador_2.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    00:22:11 04/18/2017 
+-- Design Name: 
+-- Module Name:    procesador_2 - arquitectura_procesador_2 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity procesador_2 is
+    Port ( rst : in  STD_LOGIC;
+           clk : in  STD_LOGIC;
+           s_procesador_2 : out  STD_LOGIC_VECTOR (31 downto 0));
+end procesador_2;
+
+architecture arquitectura_procesador_2 of procesador_2 is
+
+-- señales del procesador
+
+
+
+begin
+
+
+end arquitectura_procesador_2;
+
